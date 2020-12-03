5f6a0a9f48945d327474e359079d28b8b2a1228d
xoliio <xoliio@gmail.com>
2020-12-03 13:38:39 -0800

    adds basic html, user and entries class js files

---
+++ src/entries.js
@@ -0,0 +1,13 @@
+class Entry(){
+
+    constructor(date, feather, stone, id, user_id){
+        this.date = date
+        this.feather = feather
+        this.stone = stone
+        this.id = id
+        this.user_id = user_id
+    }
+
+
+
+}
